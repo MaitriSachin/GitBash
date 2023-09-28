3a7cb7769732b8e668ead6856db3e67dae100346
https://github.com/MaitriSachin/GitBash.git <maitrimnaik@gmail.com>
2023-09-28 13:46:35 +0530

    1st commit

---
+++ src/test/java/GitBashPackage/GitTest.java
@@ -0,0 +1,13 @@
+package GitBashPackage;
+
+import org.testng.annotations.Test;
+
+public class GitTest {
+	
+	@Test
+	public void gitBash()
+	{
+		System.out.println("Welcome to GitBash");
+	}
+
+}
